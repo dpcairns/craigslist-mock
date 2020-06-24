be571fde1922b4d0c8f81d90a1fa7a17d99166dd
harriet-the-spy <dpcairns@gmail.com>
2020-06-24 14:37:42 -0700

    put input into div

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const myButton = document.getElementById('cool-button');
 myButton.addEventListener('click', () => {
     const myInput = document.getElementById('cool-input');
     const userInput = myInput.value;
-
-    console.log(userInput);
     const coolDiv = document.getElementById('cool-div');
 
-    console.log(coolDiv);
+    coolDiv.textContent = userInput;
 });
