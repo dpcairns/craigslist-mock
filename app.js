27d9e224d63ae4cbc196b0390e89dfe3bfd302c7
harriet-the-spy <dpcairns@gmail.com>
2020-06-24 14:36:16 -0700

    get cool div

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const myButton = document.getElementById('cool-button');
 
 myButton.addEventListener('click', () => {
     const myInput = document.getElementById('cool-input');
+    const userInput = myInput.value;
 
-    console.log(myInput.value);
+    console.log(userInput);
+    const coolDiv = document.getElementById('cool-div');
+
+    console.log(coolDiv);
 });
